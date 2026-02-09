502181e97aae7f3e6d61dd3874f3f54d320ddca4
SeaBassy4 <sebastianperezgonzalez4@gmail.com>
2026-02-08 20:34:07 -0700

    chore: orc idle and walking animations + player  camera adjustment and jump height lowered

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 const SPEED = 150.0
-const JUMP_VELOCITY = -600.0
+const JUMP_VELOCITY = -500.0
 
 # Variable para controlar si está vivo
 var esta_vivo = true
--- path_follow_2d.gd
+++ path_follow_2d.gd
@@ -1,11 +1,42 @@
 extends PathFollow2D
-var velocidad = 0.3
+
+var velocidad := 0.2
 var ultima_posicion: float = 0.0
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+# Estados
+var estado := "idle"   # "idle" | "walk"
+var tiempo_estado := 0.0
+
+func _ready():
+	# Tiempo inicial en idle
+	tiempo_estado = randf_range(1.0, 3.0)
+	$Enemigo/AgentAnimatorEnemy/AnimationPlayer.play("idle")
+
 func _process(delta):
-	progress_ratio += velocidad * delta
-	if position.x < ultima_posicion:
-		$Enemigo/AgentAnimatorEnemy/Sprite2D.flip_h = true #mueve a la izquierda
-	elif position.x > ultima_posicion:
-		$Enemigo/AgentAnimatorEnemy/Sprite2D.flip_h = false #mueve a la derecha
-	ultima_posicion = position.x
+	tiempo_estado -= delta
+
+	if estado == "idle":
+		# No se mueve
+		if tiempo_estado <= 0.0:
+			# Cambiar a caminar
+			estado = "walk"
+			tiempo_estado = randf_range(2.0, 4.0)
+			$Enemigo/AgentAnimatorEnemy/AnimationPlayer.play("caminando")
+
+	elif estado == "walk":
+		# Moverse por el path
+		progress_ratio += velocidad * delta
+
+		# Flip según dirección
+		if position.x < ultima_posicion:
+			$Enemigo/AgentAnimatorEnemy/Sprite2D.flip_h = true
+		elif position.x > ultima_posicion:
+			$Enemigo/AgentAnimatorEnemy/Sprite2D.flip_h = false
+
+		ultima_posicion = position.x
+
+		if tiempo_estado <= 0.0:
+			# Volver a idle
+			estado = "idle"
+			tiempo_estado = randf_range(1.0, 3.0)
+			$Enemigo/AgentAnimatorEnemy/AnimationPlayer.play("idle")
